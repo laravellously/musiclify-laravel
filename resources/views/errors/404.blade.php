c06e5e075fdff8b5236514af362205d0c56898ff
stevekeyz <stephenbitrus01@gmail.com>
2022-12-22 05:39:01 +0100

    5:38AM

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -27,11 +27,11 @@
 
 </head>
 <body class="h-full">
-    
+
     <div class="bg-gray-50 min-h-full px-4 py-16 sm:px-6 sm:py-24 md:grid md:place-items-center lg:px-8">
         <div class="max-w-max mx-auto">
             <main class="sm:flex">
-                <p class="text-4xl font-extrabold text-primary-600 sm:text-5xl">404</p>
+                <p class="text-4xl font-extrabold text-primary-600 sm:text-5xl">FOUR OH! FOUR</p>
                 <div class="sm:ml-6">
                     <div class="sm:border-l sm:border-gray-200 sm:pl-6">
                         <h1 class="text-2xl font-extrabold text-gray-900 sm:text-3xl uppercase tracking-widest">
@@ -43,10 +43,10 @@
                     </div>
                     <div class="mt-10 flex space-x-3 sm:border-l sm:border-transparent sm:pl-6">
                         <a href="{{ url('/') }}" class="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-primary-600 hover:bg-primary-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-600">
-                            @lang('messages.t_back_to_homepage')    
+                            @lang('messages.t_back_to_homepage')
                         </a>
                         <a href="{{ url('help/contact') }}" class="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-primary-700 bg-indigo-100 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-600">
-                            @lang('messages.t_contact_us')    
+                            @lang('messages.t_contact_us')
                         </a>
                     </div>
                 </div>
